d7f3df8bcd82728ad29fa2952dc23e20b4326877
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-13 17:53:59 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"catch",
+        options:["caught", "catched"],
+        answer: 0
+    },
+    {
+        q:"send",
+        options:["sended", "sent"],
+        answer: 1
+    },
+    {
+        q:"find",
+        options:["found", "finded"],
+        answer: 0
+    },
+    {
+        q:"hide",
+        options:["hided", "hid"],
+        answer: 1
+    },
+    {
+        q:"break",
+        options:["broke", "brake"],
+        answer: 0
+    },
+    {
+        q:"wake",
+        options:["waked", "woke"],
+        answer: 1
+    },
+    {
+        q:"decide",
+        options:["decided", "decade"],
+        answer: 0
+    },
+    {
+        q:"hear",
+        options:["heared", "heard"],
+        answer: 1
+    },
+    {
+        q:"leave",
+        options:["left", "leaved"],
+        answer: 0
+    },
+    {
+        q:"put",
+        options:["putted", "put"],
+        answer: 1
+    }
+
+
+
+]
